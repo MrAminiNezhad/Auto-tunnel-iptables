c829430f6fbf92e70798d1a6f4623f0d17214216
Pouria amini <43442483+MrAminiNezhad@users.noreply.github.com>
2023-10-27 17:12:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ while true; do
     echo -e "${GREEN}|${GREEN}              W E L L C O M E                 ${GREEN}|${NC}"
     echo -e "${BLUE}|${GREEN}                    T O                         ${BLUE}|${NC}"
     echo -e "${BLUE}|${GREEN}      A u t o t u n n e l i p t a b e l         ${BLUE}|${NC}"
-    echo -e "${BLUE}|                                        ${RED}ver 1.2 ${NC}  ${BLUE}|${NC}"
+    echo -e "${BLUE}|                                        ${RED}ver 2.0 ${NC}  ${BLUE}|${NC}"
     echo -e "${BLUE}|${NC}                       B Y                      ${BLUE}|${NC}"
     echo -e "${BLUE}|${NC}                  MR.AminiNezhad                ${BLUE}|${NC}"
     echo -e "${BLUE}|           ---------------------------            |${NC}"
@@ -33,7 +33,6 @@ while true; do
     echo -e "${BLUE}|                                                  |${NC}"
     echo -e "${BLUE}|${YELLOW} 1.${NC} ${CYAN}Installing Iptable with BBR${NC}         ${BLUE}|${NC}"
     echo -e "${BLUE}|${YELLOW} 2.${NC} ${GRAY}Installing Iptable with Hybla${NC}       ${BLUE}|${NC}"
-    echo -e "${BLUE}|${YELLOW}3.${NC} ${GREEN}CREDITS${NC}                                       ${BLUE}|${NC}"
     echo -e "${BLUE}|${YELLOW}0.${NC} ${RED}QUIT${NC}                                          ${BLUE}|${NC}"
     read -p "Enter option number: " choice
     
@@ -41,20 +40,6 @@ while true; do
         wget -N --no-check-certificate https://raw.githubusercontent.com/MrAminiNezhad/Auto-tunnel-iptables/main/run2.sh && chmod +x run2.sh && sudo bash run2.sh
     elif [ "$choice" == "2" ]; then
         wget -N --no-check-certificate https://raw.githubusercontent.com/MrAminiNezhad/Auto-tunnel-iptables/main/run.sh && chmod +x run.sh && sudo bash run.sh
-    elif [ "$choice" == "3" ]; then
-        clear
-        echo "########################################"
-        echo "#                                      #"
-        echo "#                Credits               #"
-        echo "#                                      #"
-        echo "########################################"
-        echo ""
-        echo "This bash was created by Soroush Nekoozadeh"
-        echo "Special thanks to AtomicBoys Team, alex, and kimi for their contributions."
-        echo "This script has been made to make your job faster. So use it appropriately."
-        echo "I used a lot of people's code, scripts, and more, which would take a whole day to name. You can find them on GitHub."
-        echo "Press ENTER to continue"
-        read -s -n 1
     elif [ "$choice" == "0" ]; then
         echo "Exiting..."
         echo "Exiting program"

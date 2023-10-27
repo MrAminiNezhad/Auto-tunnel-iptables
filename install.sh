6904093bc771097b78ea28a88c19788773575b4c
Pouria amini <43442483+MrAminiNezhad@users.noreply.github.com>
2023-10-27 16:59:40 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,29 +23,29 @@ while true; do
 
     if [ "$choice" == "1" ]; then
         wget -N --no-check-certificate https://raw.githubusercontent.com/MrAminiNezhad/Auto-tunnel-iptables/main/run2.sh && chmod +x run2.sh && sudo bash run2.sh
-elif [ "$choice" == "2" ]; then
-    wget -N --no-check-certificate https://raw.githubusercontent.com/MrAminiNezhad/Auto-tunnel-iptables/main/run.sh && chmod +x run.sh && sudo bash run.sh
-elif [ "$choice" == "3" ]; then
-    clear
-    echo "########################################"
-    echo "#                                      #"
-    echo "#                Credits               #"
-    echo "#                                      #"
-    echo "########################################"
-    echo ""
-    echo "This bash was created by Soroush Nekoozadeh"
-    echo "Special thanks to AtomicBoys Team, alex, and kimi for their contributions."
-    echo "This script has been made to make your job faster. So use it appropriately."
-    echo "I used a lot of people's code, scripts, and more, which would take a whole day to name. You can find them on GitHub."
-    echo "Press ENTER to continue"
-    read -s -n 1
-elif [ "$choice" == "0" ]; then
-    echo "Exiting..."
-    echo "Exiting program"
-    exit 0
-else
-    echo "Invalid option. Please choose a valid option."
-    echo "Press ENTER to continue"
-    read -s -n 1
-fi
+    elif [ "$choice" == "2" ]; then
+        wget -N --no-check-certificate https://raw.githubusercontent.com/MrAminiNezhad/Auto-tunnel-iptables/main/run.sh && chmod +x run.sh && sudo bash run.sh
+    elif [ "$choice" == "3" ]; then
+        clear
+        echo "########################################"
+        echo "#                                      #"
+        echo "#                Credits               #"
+        echo "#                                      #"
+        echo "########################################"
+        echo ""
+        echo "This bash was created by Soroush Nekoozadeh"
+        echo "Special thanks to AtomicBoys Team, alex, and kimi for their contributions."
+        echo "This script has been made to make your job faster. So use it appropriately."
+        echo "I used a lot of people's code, scripts, and more, which would take a whole day to name. You can find them on GitHub."
+        echo "Press ENTER to continue"
+        read -s -n 1
+    elif [ "$choice" == "0" ]; then
+        echo "Exiting..."
+        echo "Exiting program"
+        exit 0
+    else
+        echo "Invalid option. Please choose a valid option."
+        echo "Press ENTER to continue"
+        read -s -n 1
+    fi
 done

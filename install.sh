b7c61ca4e28ea3cc9b33ef80c3b315c6675a2bd8
Pouria amini <43442483+MrAminiNezhad@users.noreply.github.com>
2023-10-27 16:53:23 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,45 @@ while true; do
     echo -e "${BLUE}|                                                  |${NC}"
     echo -e "${BLUE}|${YELLOW} 1.${NC} ${CYAN}Installing Iptable with BBR${NC}         ${BLUE}|${NC}"
     echo -e "${BLUE}|${YELLOW} 2.${NC} ${GRAY}Installing Iptable with Hybla${NC}       ${BLUE}|${NC}"
-
-read -p "Enter your choice (1 or 2): " choice
+    echo -e "${BLUE}|${YELLOW}3.${NC} ${GREEN}CREDITS${NC}                                       ${BLUE}|${NC}"
+    echo -e "${BLUE}|${YELLOW}0.${NC} ${RED}QUIT${NC}                                          ${BLUE}|${NC}"
+    read -p "Enter option number: " choice
 
 if [ "$choice" == "1" ]; then
     wget -N --no-check-certificate https://raw.githubusercontent.com/MrAminiNezhad/Auto-tunnel-iptables/main/run2.sh && chmod +x run2.sh && sudo bash run2.sh
 elif [ "$choice" == "2" ]; then
     wget -N --no-check-certificate https://raw.githubusercontent.com/MrAminiNezhad/Auto-tunnel-iptables/main/run.sh && chmod +x run.sh && sudo bash run.sh
-else
-    echo "Invalid choice. Please enter 1 or 2."
+elif [ "$choice" == "3" ]; then
+            clear
+            echo ""
+            echo -e "${BLUE}########################################${NC}"
+            echo -e "${BLUE}#${NC}                                      ${BLUE}#${NC}"
+            echo -e "${BLUE}#${NC}                ${YELLOW}Credits${NC}             ${BLUE}#${NC}"
+            echo -e "${BLUE}#${NC}                                      ${BLUE}#${NC}"
+            echo -e "${BLUE}########################################${NC}"
+            echo ""
+
+            # Print contributors
+            echo -e "This bash  was created by ${GREEN}Soroush Nekoozadeh${NC}\n"
+            echo -e "Special thanks to ${GREEN}AtomicBoys Team${NC}, ${GREEN}alex${NC}, and ${GREEN}kimi${NC} for their contributions.\n"
+            echo -e "this script only has been made to  make youre job faster. so use it appropriately.i used lot of people codes scripts and .... that takes hole day to name. you can find them in github ^_^"
+            # Wait for user input
+            echo -e "Press ${RED}ENTER${NC} to continue"
+            read -s -n 1
+            ;;
+elif [ "$choice" == "0" ]; then
+            echo ""
+            echo -e "${GREEN}Exiting...${NC}"
+            echo "Exiting program"
+            exit 0
+            ;;
+elif [ "$choice" == "*" ]; then
+           echo "Invalid option. Please choose a valid option."
+           echo ""
+           echo -e "Press ${RED}ENTER${NC} to continue"
+           read -s -n 1
+           ;;
 fi
+      esac
+done
+       
